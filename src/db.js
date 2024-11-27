c3b71374191989b8bbeeb309e6671d70d05cba82
vvthangdev <vvthang.dev@gmail.com>
2024-11-27 08:39:32 +0700

    change sth

--- src/db.js
+++ src/db.js
@@ -4,5 +4,6 @@ const {MongoClient} = require("mongodb");
 const client = new MongoClient(process.env.MONGODB_CONNECT_STRING, {
     useUnifiedTopology: true,
 });
+
 const connection = client.connect();
 module.exports = connection;
